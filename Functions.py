b366c50013b719120e41a77dd38659a8434a12f9
oblivion <59708737+2-718281@users.noreply.github.com>
2022-01-17 22:06:01 +0000

    map method update
    - map gen method upadte
    - validation check added

--- Functions.py
+++ Functions.py
@@ -1,2 +1,9 @@
 def dictionary(a, b):
     pass
+
+
+def find_centre(h, w):
+    centre_x = h / 2
+    centre_y = w / 2
+
+    return centre_x, centre_y
--- map.py
+++ map.py
@@ -3,88 +3,107 @@ import pygame as pg
 import random
 import random as rd
 
+# constants will go to constants.py
 DEFAULT_ROOM_TYPE = 0
-DEFAULT_ROOM_WIDTH = 30
-DEFAULT_ROOM_HEIGHT = 30
-
-TRAP_TRUE_PROBABILITY = 0.5
+DEFAULT_ROOM_WIDTH = 10
+DEFAULT_ROOM_HEIGHT = 10
+TRAP_TRUE_PROBABILITY = 0.3
 
 
 class Room:
+    # tile 0 == empty, 1 == solid platform, 2 == trap platform, 3 == , 4 == spawning tile, 5 == destination tile
     _next_room_ID = 0
     _room_dict = {}
 
-    def __init__(self):
-        self.room_type = DEFAULT_ROOM_TYPE
-        self.room_height = DEFAULT_ROOM_WIDTH
-        self.room_width = DEFAULT_ROOM_HEIGHT
-        self.d_room_config = np.zeros((self.room_width, self.room_height))
+    def __init__(self, room_width=DEFAULT_ROOM_WIDTH, room_height=DEFAULT_ROOM_HEIGHT, room_type=DEFAULT_ROOM_TYPE):
+        self.room_type = room_type
+        self.room_height = room_height
+        self.room_width = room_width
+        self.d_room_config = np.zeros((room_height, room_width))  # type 0 rooms : are filled with many 0s
 
-        self.room_config = self.d_room_config
-        self.floor = np.ones((self.room_width, 1))
+        self.room_config = self.d_room_config.copy()  # copy the default room 0 to new room template
         self.room_ID = Room._next_room_ID
+        # self.room_area = self.room_width * self.room_height  replaced by the calc_area function
         Room._next_room_ID += 1
 
-    def default_generation(self, room_type):  # Generate room base on the type of room
+        self.new_room(room_type)  # generate new room object
 
+    def default_generation(self, room_type):  # Generate room base on the type of room :1,2,3
+        room = self.d_room_config.copy()
         if room_type == 1:  # Setup a starting point at the 2nd row, break wall at last row
-            room = self.room_config
+            room[0, :] = 1
+            room[self.room_height - 1, :] = 1
+            room[:, 0] = 1
+            room[self.room_height - 2, 1] = 4
 
-            room[0] = self.floor
-            room[self.room_width] = self.floor
-            room = room.T
-            room[0] = self.floor
-            room = room.T
-
-        elif room_type == 2:  # break wall and 1st and last row
-            room = self.room_condig
-            floor = np.ones(self.room_width, 1)
-            room[0] = floor
-            room[self.room_width] = floor
+        elif room_type == 2:  # a tube like room
+            room[0, :] = 1
+            room[self.room_height - 1, :] = 1
 
         elif room_type == 3:  # break 1st wall, setup a destination at 2nd last row
-            room = self.room_config
-            floor = np.ones(self.room_width, 1)
-            room[0] = floor
-            room[self.room_width] = floor
-            room = room.T
-            room[self.room_height] = floor
-            room = room.T
-
+            room[0, :] = 1
+            room[self.room_width, :] = 1
+            room[self.room_height - 1, :] = 1
+            room[self.room_height - 2, self.room_width - 2] = 5
         return room
 
     def new_room(self, room_type):  # Generate a new room
 
-        new_room = self.default_generation(room_type)
-        self.room_config = new_room
+        self.room_config = self.default_generation(room_type)
+        self.platfrom_generation()
+        self.trap_generation()
         Room._room_dict[self.room_ID] = self.room_config  # add new room to the room dictionary
 
-    def room_update(self):
-        pass
+    def calc_room_area(self):
+        return np.sum((self.room_config == 0).astype(np.int32))   # sum up all None-0 cells to work out area
 
-    def platfrom_generation(self):  # put new platform onto a blank array, then do a AND operation
-        room_area = self.room_height * self.room_width
-        for i in range(0, self.room_height):
-            if room_area < 0.5(self.room_width * self.room_height):
-                length = rd.randint(0, self.room_width)
-                start_pos = rd.randint(0, self.room_width - length)
-                new_plat = np.ones(self.length,1)
-                
+    def platfrom_generation(self):  # put solid platform into the room
+        print("generating platform")
+        candidate_platform_number = rd.randint(self.room_height // 2+4, self.room_height - 1)
+        # random numbers of platform
+
+        for i in range(1, candidate_platform_number):
+            room_area = self.calc_room_area()
+
+            if room_area > 0.6 * (self.room_width * self.room_height):
+
+                for k in range(5):  # try max 5 times
+                    length = rd.randint(3, self.room_width // 2)
+                    start_pos_x = rd.randint(1, self.room_width - length - 2)
+                    center_pos_x = start_pos_x + length // 2
+                    center_pos_y = rd.randint(1, self.room_height - 2)
+                    result = Room.validation_check(self.room_config, [center_pos_x, center_pos_y], length, radius=2)
+
+                    if result:
+                        self.room_config[center_pos_y, start_pos_x:start_pos_x + length] = 1
+                        print('platform added')
+                        break
             else:
-                pass
+                break
 
     def trap_generation(self):  # generate trap upon platform
-        for i in self.room_config:
-            for j in self.room_config[i]:
-                if j == 1:
-                    chance = random.Random   # there is a 50% chance of generating 1 trap
-                    if chance >= TRAP_TRUE_PROBABILITY:
-                        pass
-                    else:
-                        self.room_config = 2  # 2 represent trap
+        print("generate traps")
+        for i in range(1, self.room_height - 1):
+            for j in range(1, self.room_width - 1):
+                if self.room_config[i][j] == 1:
+                    chance = random.random()  # there is a 50% chance of generating 1 trap
+                    if chance < TRAP_TRUE_PROBABILITY:
+                        self.room_config[i][j] = 2
 
-    def validation_check(self):  # check if there is at least 1 valid route
-        pass
+    @staticmethod
+    def validation_check(candidate_map, center_pos, platform_length,
+                         radius=3):  # check if there is at least 1 valid route
+        roi_left = max(0, center_pos[0] - platform_length // 2 - radius)
+        roi_right = min(candidate_map.shape[1], center_pos[0] + platform_length // 2 + radius + 1)
+        roi_down = max(0, center_pos[1] - radius)
+        roi_up = min(candidate_map.shape[0], center_pos[1] + radius + 1)
+
+        count_occupied = np.sum((candidate_map[roi_down:roi_up, roi_left:roi_right] > 0).astype(np.int32))
+        # print(count_occupied)
+        return count_occupied == 0
 
 
-
+r1 = Room(room_width=10, room_height=10, room_type=1)
+print(r1.room_config)
+r2 = Room(room_width=20, room_height=20, room_type=1)
+print(r2.room_config)
