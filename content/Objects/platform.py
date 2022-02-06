b20736ef4b5734582dc5fcd95f45f3fb7a5a9497
Qianli Xia <qx211@roe.ac.uk>
2022-02-07 07:57:28 +0800

    Update platform.py

--- content/Objects/platform.py
+++ content/Objects/platform.py
@@ -3,13 +3,14 @@ from content.Tools import constant as c
 
 
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, h, w):
+    def __init__(self, x, y, h, w, index):  # 平台类，坐标，长宽
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
+        self.image = pg.Surface((w, h))  # 长宽
         self.image.fill(c.BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.index = index
 
 
 class Starting(pg.sprite.Sprite):
