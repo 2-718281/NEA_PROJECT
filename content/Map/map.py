12d50c92fde8c3b24ae0bf26d4bd5b8fc4e62a99
oblivion <59708737+2-718281@users.noreply.github.com>
2022-02-06 09:44:17 +0000

    commit

--- content/Map/map.py
+++ content/Map/map.py
@@ -1,27 +1,31 @@
 import numpy as np
 import random as rd
+from content.Tools import Functions as F
 
 MAP_WIDTH = 4
 MAP_HEIGHT = 4
 
 
-class Mini_Map:
+class big_map:
+
     _next_ID = 0
 
     def __init__(self):
-        self.Map_ID = Mini_Map._next_ID
-        Mini_Map._next_ID += 1
-        self.MapArr = np.zeros((MAP_HEIGHT * MAP_WIDTH, MAP_HEIGHT * MAP_WIDTH))
+        self.Map_ID = big_map._next_ID
+        big_map._next_ID += 1
+        self.nodes = MAP_HEIGHT * MAP_WIDTH
+        self.map = F.Graph(self.nodes)
 
-    def map_gen(self):
+    def gene_proc(self):
+        start_node = 0
         prob = 0.5
-        pos = (0, 0)
-        while pos[0] <= MAP_WIDTH and pos[1] <= MAP_HEIGHT:
+        terminate = False
+        while not terminate:
+            nodelist = [start_node+1,]
+            next_prob = rd.random()
+            if next_prob <= prob:
+                
 
-            next_pos_prob = rd.random()
 
-            if next_pos_prob >= prob:  # shift down
-                pos[0] += 1
-                self.MapArr[pos[0]]
-            else:  # otherwise shift leftward
-                pos[1] += 1
+
+
--- content/Tools/Functions.py
+++ content/Tools/Functions.py
@@ -1,1 +1,16 @@
 # random functions goes here
+
+class Graph:
+
+    def __init__(self, node_num):
+        self.nodes = node_num
+        self.adjmat = [[0 for i in range(self.nodes)] for i in range(self.nodes)]
+
+    def add_link(self, nodeA, nodeB):
+        self.adjmat[nodeA,nodeB] = 1
+        self.adjmat[nodeB,nodeA] = 1
+
+    def del_link(self, nodeA, nodeB):
+        self.adjmat[nodeA,nodeB] = 0
+        self.adjmat[nodeB,nodeA] = 0
+
