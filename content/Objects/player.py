81569a59f13e3373f5a1e7ae4b255aee045d5764
oblivion <59708737+2-718281@users.noreply.github.com>
2022-02-07 01:01:38 +0000

    commit

--- content/Objects/player.py
+++ content/Objects/player.py
@@ -16,14 +16,14 @@ class Player(pg.sprite.Sprite):  # inherit from sprite
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)  # 初始化sprite
         self.game = game
-        self.image = pg.Surface((50, 50))
+        self.image = pg.Surface((c.ROOM_TO_GUI_SCALE,c.ROOM_TO_GUI_SCALE))
         self.image.fill(c.GREEN)
         self.rect = self.image.get_rect()  # 获取正方形 get rect
         self.rect.center = (c.WIDTH/2, c.HEIGHT/2)
         self.pos = vec(c.WIDTH / 2, c.HEIGHT / 2)  # 初始化位置矢量 position vector
         self.vel = vec(0, 0)  # 初始化速度矢量 velocity vector
         self.acc = vec(0, 0)  # 初始化加速度矢量 acceleration vector
-        self.HP = 10
+        self.HP = 10000
         self.state = s.stand
 
     def jump(self):
@@ -42,19 +42,29 @@ class Player(pg.sprite.Sprite):  # inherit from sprite
             self.acc.x = -c.P_ACC
         elif keys[pg.K_d]:  # move right
             self.acc.x = c.P_ACC
-
         self.acc.x += self.vel.x * c.P_FRI  # add friction in x direction
         self.vel += self.acc  # v = u + at
         self.pos += self.vel + 0.5 * self.acc  # s = ut + 1/2 vt^2
-
         self.rect.midbottom = self.pos  # for the collision
+        self.change_state()
+        self.check_state()
+
+    def change_state(self):
+        if self.vel == vec(0,0):
+            self.state = s.stand
+        else:
+            self.state = s.move
+
 
     def check_state(self):
         if self.state == s.super:
             HP = self.HP
             self.HP = 999999999999999
-            time.sleep(1)
-            self.HP = HP
+            self.image.fill(c.RED)
+
+        elif self.state == s.move:
+            self.image.fill(c.GREEN)
+
 
     def dead(self):
         if self.pos.y >= c.HEIGHT:
